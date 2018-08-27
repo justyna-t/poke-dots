40d9ade24d06f4fbeff935f2a0451638ba00a9b0
justyna-t <justyna.anita.kaminska@gmail.com>
2018-08-27 12:26:39 +0200

    Started to create Poke the Dots Version 1

---
+++ poke1.py
@@ -0,0 +1,48 @@
+# Poke the Dots V1
+# This is a graphical game where two dots move around the screen, bouncing off
+# the edges.
+
+
+# def main
+    # create window
+    # create game
+        # create clock
+        # create small dot
+        # create big dot
+    # play game
+    # close window
+
+
+# def play game
+    # while not player has selected close
+        # play frame
+            # handle events
+            # draw game
+            # update game
+
+
+# def handle events
+    # for event in event list
+        # handle one event
+            # if event category equals close
+                # remember player has selected close
+
+
+# def draw game
+    # draw small dot
+    # draw big dot
+    # update display
+
+
+# def update game
+    # move small dot
+    # move big dot
+    # control frame rate
+
+
+# def move dot
+    # for index in sequence o to 1
+        # update center at index
+            # add velocity at index to center at index
+            # if dot edge outside window
+                # negate velocity at index
