48d56db30af84a41681c38cc88ad9b7bd27137c6
justyna-t <justyna.anita.kaminska@gmail.com>
2018-08-29 11:15:42 +0200

    Comments have been updated

--- poke2.py
+++ poke2.py
@@ -9,6 +9,8 @@ from pygame.time import Clock
 from pygame.event import get as get_events
 from pygame.draw import circle as draw_circle
 
+# User-defined functions
+
 
 def main():
     window = create_window()
@@ -19,16 +21,19 @@ def main():
 
 def create_window():
     # Create a window for the game, open it, and return it.
+
     window = Window("Poke the Dots", 500, 400)
     window.set_bg_color("black")
     return window
 
 
 def create_game(window):
-    #  Create a Game object for Poke the Dots
+    # Create a Game object for Poke the Dots.
+    # - window is the Window that the game is played in
+
     game = Game()
     game.window = window
-    game.frame_rate = 90 # larger is faster game
+    game.frame_rate = 90  # larger is faster game
     game.close_selected = False
     game.clock = Clock()
     game.small_dot = create_dot("red", [50, 100], 30, [1, 2])
@@ -37,6 +42,14 @@ def create_game(window):
 
 
 def create_dot(color, center, radius, velocity):
+    # Create a Dot object for Poke the Dots.
+    # - color is the str color of the dot
+    # - center is a list containing the x and y int coords of the center
+    # of the dot
+    # - radius is the int pixel radius of the dot
+    # - velocity is the list of horizontal int and vertical int speeds of
+    # the dot
+
     dot = Dot()
     dot.color = color
     dot.center = center
@@ -48,20 +61,17 @@ def create_dot(color, center, radius, velocity):
 def play_game(game):
     # Play the game until the player presses the close icon.
     # - game is the Game to play
+
     while not game.close_selected:
         # play frame
-        #    handle events
         handle_events(game)
-        #   draw game
         draw_game(game)
-        #   update game
         update_game(game)
 
 
 def handle_events(game):
-    # Handle the current game events. Return True if the player clicked the
-    # close icon and False otherwise.
-    # - game is the Game whose events should be handled
+    # Handle the current game events by changing the game state appropriately.
+    # - game is the Game whose events will be handled
     event_list = get_events()
     for event in event_list:
         # handle one event
@@ -71,22 +81,19 @@ def handle_events(game):
 
 def draw_game(game):
     # Draw all game objects.
-    # - game
+    # - game is the Game to draw for
+
     game.window.clear()
-    # draw small dot
     draw_dot(game.window, game.small_dot)
-
-    # draw big dot
     draw_dot(game.window, game.big_dot)
-
-    # update display
     game.window.update()
 
 
 def draw_dot(window, dot):
     # Draw the dot on the window.
-    # - window
-    # - dot is the Dot to be drawn
+    # - window is the Window that the game is played in
+    # - dot is the Dot to draw
+
     surface = window.get_surface()
     color = Color(dot.color)
     draw_circle(surface, color, dot.center, dot.radius)
@@ -95,21 +102,20 @@ def draw_dot(window, dot):
 def update_game(game):
     # Update all game objects with state changes that are not due to
     # user events.
-    # - game
+    # - game is the Game to update
 
-    # move small dot
     move_dot(game.window, game.small_dot)
-    # move big dot
     move_dot(game.window, game.big_dot)
     # control frame rate
     game.clock.tick(game.frame_rate)
 
 
 def move_dot(window, dot):
-    # Change the location and the velocity of the dot so it remains on the
+    # Change the location and the velocity of the Dot so it remains on the
     # surface by bouncing from its edges.
-    # - window
-    # - dot is the Dot to be moved
+    # - window is the Window that the game is played in
+    # - dot is the Dot to move
+
     size = [window.get_width(), window.get_height()]
     for index in range(2):
         # update center at coordinate
@@ -123,10 +129,10 @@ def move_dot(window, dot):
 
 class Game:
     # An object in this class represents a complete game
-    # - window is the Window to play in
+    # - window
     # - frame_rate
     # - close_selected
-    # - clock is a Clock used to control game speed
+    # - clock
     # - small_dot
     # - big_dot
     pass
@@ -134,12 +140,10 @@ class Game:
 
 class Dot:
     # An object in this class represents a dot
-    # - color is the str color of the dot
-    # - center is the list of x int and y int coordinates of the center of
-    # the dot
-    # - radius is the int radius of the dot
-    # - velocity is the list of horizontal int and vertical int speeds of
-    # the dot
+    # - color
+    # - center
+    # - radius
+    # - velocity
     pass
 
 
